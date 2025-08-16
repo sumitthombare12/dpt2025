37244d383288487d9440c49b0461d9ca867ed1d1
sumitthombare12 <sumitthombare759@gmail.com>
2025-08-16 12:02:32 +0530

    Create Day2.cpp
    You are given an array arr containing n-1 distinct integers. The array consists of integers taken from the range 1 to n, meaning one integer is missing from this sequence. Your task is to find the missing integer.

---
+++ Day2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 0;
+    
+    //INPUT
+    cout<<"Enter n:";
+    cin>>n;
+    
+    int arr[n-1];
+    cout<<"Enter a "<<n-1<<" Element in array(1 to n):"<<endl;
+    
+    for(int i=0; i<n-1; i++) {
+        cin>>arr[i];
+    }
+    
+    //*** MAIN CODE ****
+    int expected_sum = (n*(n+1))/2;
+    int curr_sum = 0;
+    
+    for(int &val: arr) {
+        curr_sum += val;
+    }
+    
+    cout<<"Output:"<<expected_sum-curr_sum;
+    return 0;
+}
