3e35d918ced1aa66c71127aeb8d11f8311344f3f
sumitthombare12 <sumitthombare759@gmail.com>
2025-08-17 21:33:05 +0530

    Create Day3.cpp
    Problem : Find the Duplicate Number
    You are given an array arr containing n+1 integers, where each integer is in the range [1, n] inclusive. There is exactly one duplicate number in the array, but it may appear more than once. Your task is to find the duplicate number without modifying the array and using constant extra space.

---
+++ Day3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    //INPUT
+    int n = 0;
+    cout<<"Enter a n:";
+    cin>>n;
+    
+    int arr[n+1];
+    cout<<"Enter n+1 element from 1 to n:"<<endl;
+    for(int i=0; i<=n; i++) {
+        cout<<"Element "<<i<<":";
+        cin>>arr[i];
+    }
+    
+    // *** MAIN CODE ***
+    
+    int slow = arr[0];
+    int fast = arr[0];
+    
+    slow = arr[slow];
+    fast = arr[arr[fast]];
+    
+    while(slow != fast) {
+        slow = arr[slow];
+        fast = arr[arr[fast]];
+    }
+    
+    slow = arr[0];
+    
+    while(slow != fast) {
+        slow = arr[slow];
+        fast = arr[fast];
+    }
+    
+    //OUTPUT
+    cout<<"OUTPUT:"<<slow;
+    return 0;
+}
