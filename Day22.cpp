a2bd9c15b6eb45fe3745989fcc2d3bdd9974bb6f
sumitthombare12 <sumitthombare759@gmail.com>
2025-09-05 18:17:56 +0530

    First Element to Repeat k Times.cpp
    You are given an array of integers and an integer k. Your task is to find the first element in the array that appears exactly k times. If no such element exists, return -1.
    
    Input :
    * An integer array arr of size n, where 1≤n≤105
    * An integer k, where 1≤k≤n
    * Input: arr = [3, 1, 4, 4, 5, 2, 6, 1, 4], k = 2
    
    Output :
    Return the first element from the array that occurs exactly k times. If no element occurs exactly k times, return -1.
    Output: 1

---
+++ Day22.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+    
+    int n = 0;
+    cout<<"Enter size:";
+    cin>>n;
+    
+    long long arr[n];
+    
+    cout<<"Enter element:";
+    for(int i=0; i<n; i++) {
+        cin>>arr[i];
+    }
+    
+    long long k;
+    cout<<"Enter k:";
+    cin>>k;
+    
+    unordered_map<long, long> mp;
+    for(int i=0; i<n; i++) {
+        mp[arr[i]]++;
+    }
+    
+    for(int i=0; i<n; i++) {
+        if(mp[arr[i]] == k) {
+            cout<<arr[i];
+            return 0;
+        }
+    }
+    
+    cout<<-1;
+    return 0;
+}
